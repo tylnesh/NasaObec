cc84b2ac36e25976bffc3a8ec068cf36daa52e88
Michal Kohutek <michal@kohutek.eu>
2022-10-10 17:42:33 +0200

    Initial Commit

---
+++ NasaObec/NasaObecApp.swift
@@ -0,0 +1,17 @@
+//
+//  NasaObecApp.swift
+//  NasaObec
+//
+//  Created by Michal Kohútek on 10/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct NasaObecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
